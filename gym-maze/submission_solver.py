6bdd9dc42db318761c7b837b6796b1ee937d824d
Anton Ashraf <51335125+AntonAshraf@users.noreply.github.com>
2023-03-18 04:58:29 +0200

    last edit before the cermony

--- gym-maze/submission_solver.py
+++ gym-maze/submission_solver.py
@@ -6,6 +6,10 @@ import json
 import requests
 
 from riddle_solvers import *
+import logging
+
+logging.basicConfig(filename=r"D:\Github\DigitalSquad\gym-maze\logging.log", level=logging.DEBUG, filemode='w')
+logging.debug('This message should appear on the console') 
 
 ### the api calls must be modified by you according to the server IP communicated with you
 #### students track --> 16.170.85.45
@@ -47,13 +51,9 @@ End_path = []
 #     else:
 #       a += 1
 
-def take_desicion(steps_back, rescue_items):
-  score_now = ((rescue_items *  250) * (rescue_items/steps)) * 0.8
-  goto_exit = ((rescue_items *  250) * (rescue_items/steps + steps_back))
-  
-  
+
 def select_action(state):
-    print (state)
+    # logging.debug (state)
     global prev_state
     global flag
     global go_back
@@ -71,27 +71,29 @@ def select_action(state):
     y = state[0][1]
     N_Visited.add((x,y))
     
-    print ("init", state[0][0], prev_state, dir[x][y])
+    logging.debug (f"init {state[0][0]}, {prev_state}, { dir[x][y]} ")
     
     while (True):
       if ((x,y) == (0,0)):
         
         if (flag%2 == 0):
-          print("ZZZZZZZZZZ")
+          logging.debug("first node")
           action = actions[1]
           flag += 1
         else:
           action = actions[2]
-        print (action, (x,y))
+        
+        logging.debug (f"action ({x},{y}")
         visited [x][y] = True
         action_index = actions.index(action)
         dir[x][y] += 1 # add direction taken
         prev_state = [0,0]
-        print(action)
+        logging.debug(action)
         return action, action_index
+      
       if (state[0][0] == prev_state[0] and state[0][1] == prev_state[1] and dir[x][y] < 4): # have direction move (same location)
         go_back = False
-        print ("try another direction ",prev_state, state[0], dir[x][y])
+        logging.debug (f"try another direction {prev_state}, {state[0]}, {dir[x][y]}")
         
         next = dir[x][y]
         
@@ -99,28 +101,28 @@ def select_action(state):
         b1 = x + idx[blocked_idx][0]
         b2 = y + idx[blocked_idx][1]
         blocked[b1][b2].append((x,y))
-        print("Current: ", state[0],"blocked: ", blocked[x][y])
+        logging.debug(f"Current: { state[0]}, blocked: {blocked[x][y]}")
         # check that it is not prev
         a = x + idx[next][0]
         b = y + idx[next][1]
         
-        print("temp", temp)
+        logging.debug(f"temp: {temp}")
         if (a >= 0 and a < 10 and b >= 0 and b < 10): # if valid
           if (come_from[x][y] != [a,b]): # not comming from upcomong node
             if ((a,b) not in blocked[x][y]):
               action = actions[next]
               action_index = actions.index(action)
-              print(action)
+              logging.debug(action)
               dir[x][y] += 1 # add direction take
               return action, action_index
         dir[x][y] += 1 # add direction take
         continue
       elif ( dir[x][y] >= 4 ): # go back
-        print ("go back ",prev_state, state[0], come_from[x][y])
+        logging.debug (f"go back {prev_state} {state[0]} {come_from[x][y]}")
         prev_state = [x,y]
         z = x - come_from[x][y][0]
         s = y - come_from[x][y][1]
-        print(z,s)
+        logging.debug(f"{z},{s}")
         Rback = idx.index((z,s))
         if (Rback == 1 or Rback == 3):
           Rback -= 1
@@ -128,11 +130,11 @@ def select_action(state):
           Rback += 1
         action = actions[Rback]
         action_index = actions.index(action)
-        print(action)
+        logging.debug(action)
         return action, action_index
       elif ((state[0][0] != prev_state[0] or state[0][1] != prev_state[1]) and dir[x][y] < 4): # move
         go_back = False
-        print ("move ",state[0],prev_state,  dir[x][y])
+        logging.debug (f"move {state[0]} {state[0]} {prev_state} {dir[x][y]}")
         
         visited[x][y] = True
         if(come_from[x][y] == [-1,-1]):
@@ -143,21 +145,21 @@ def select_action(state):
         a  = x + idx[next][0]
         b  = y + idx[next][1]
         # if(come_from[x][y][0] )
-        # print (temp)
-        print("temp", temp)
+        # logging.debug (temp)
+        logging.debug(f"temp {temp}")
         if (a >= 0 and a < 10 and b >= 0 and b < 10):
           if((a,b) not in N_Visited):
             if ((a,b) not in blocked[x][y]):
               action = actions[next]
               action_index = actions.index(action)
-              print(action)
+              logging.debug(action)
               dir[x][y] += 1 # add direction taken
               return action, action_index
           dir[x][y] += 1 # add direction take
           continue
       else:
-        print ("prev",prev_state, state[0], dir[x][y] )
-        print("XXXXXXXX")
+        logging.debug ("prev {prev_state}, {state[0]}, {dir[x][y]}")
+        logging.debug("Dead End")
         # break
 
    
@@ -178,24 +180,26 @@ def get_obv_from_response(response):
     obv = [position, distances, directions] 
     return obv
 
-cnt = 0 
+steps = 0 
 riddles_counter = set()
-def submission_inference(riddle_solvers):
 
+def submission_inference(riddle_solvers):
+  
     response = requests.post(f'http://{server_ip}:5000/init', json={"agentId": agent_id})
     obv = get_obv_from_response(response)
-    global cnt
+    global steps
   
     while(True):
-      cnt +=1
-      print ("steps: ", cnt)
-      if (cnt > 51):
-        if (cnt > 50 and cnt < 92):
+      steps +=1
+      print ("steps: ", steps)
+      if (steps > 51):
+        if (steps > 50 and steps < 150):
           # Select an action
           # time.sleep(0.001)
           state_0 = obv
           action, action_index = select_action(state_0) # Random action
           response = move(agent_id, action)
+          
           with open("output.txt", "a") as f:
             print(response, file=f)
           if not response.status_code == 200:
@@ -210,22 +214,28 @@ def submission_inference(riddle_solvers):
             solution = riddle_solvers[response.json()['riddleType']](response.json()['riddleQuestion'])
             response = solve(agent_id, response.json()['riddleType'], solution)
             riddles_counter.add(response.json()['riddleType'])
-            
-          if ( response.json()['rescuedItems'] > 1):
-            print ("A&AAAAAAAAAAA11111")
+          
+          if (steps < 61 and  response.json()['rescuedItems'] > 1): # 2 riddles
+            print (f"out within 60 with 2 in {steps} steps")
+            response = requests.post(f'http://{server_ip}:5000/leave', json={"agentId": agent_id})
+            break
+          if ( response.json()['rescuedItems'] > 2):
+            print (f"out within 110 with 2 in {steps} steps")
             response = requests.post(f'http://{server_ip}:5000/leave', json={"agentId": agent_id})
             break
           
           
-        elif(cnt > 50 and cnt < 130):
+        elif(steps > 149 and steps < 222):
           state_0 = obv
           action, action_index = select_action(state_0) # Random action
           response = move(agent_id, action)
+          
           with open("output.txt", "a") as f:
             print(response, file=f)
           if not response.status_code == 200:
             print(response)
             break
+          
           obv = get_obv_from_response(response)
           print(response.json())
           
@@ -234,11 +244,30 @@ def submission_inference(riddle_solvers):
             solution = riddle_solvers[response.json()['riddleType']](response.json()['riddleQuestion'])
             response = solve(agent_id, response.json()['riddleType'], solution)
             riddles_counter.add(response.json()['riddleType'])
-          if ( response.json()['rescuedItems'] > 2):
-            print ("A&AAAAAAAAA222222")
+          
+          if ( response.json()['rescuedItems'] > 3):
+            print (f"out within 200 with 3 in {steps} steps")
             response = requests.post(f'http://{server_ip}:5000/leave', json={"agentId": agent_id})
             break
-          
+        
+        elif (steps > 221):
+          if np.array_equal(response.json()['position'], (9,9)) and response.json()['rescuedItems'] > 2:
+            response = requests.post(f'http://{server_ip}:5000/leave', json={"agentId": agent_id})
+            break
+          else:
+            state_0 = obv
+            action, action_index = select_action(state_0) # Random action
+            response = move(agent_id, action)
+            if not response.status_code == 200:
+                print(response)
+                break
+
+            obv = get_obv_from_response(response)
+            print(response.json())
+
+            if not response.json()['riddleType'] == None:
+                solution = riddle_solvers[response.json()['riddleType']](response.json()['riddleQuestion'])
+                response = solve(agent_id, response.json()['riddleType'], solution)
         else:
           print ("A& FINAL")
           response = requests.post(f'http://{server_ip}:5000/leave', json={"agentId": agent_id})
@@ -263,6 +292,7 @@ def submission_inference(riddle_solvers):
           response = solve(agent_id, response.json()['riddleType'], solution)
           riddles_counter.add(response.json()['riddleType'])
         
+
       
 
 
@@ -272,7 +302,7 @@ if __name__ == "__main__":
       print("start", file=f)
     
     
-    agent_id = "VdGcOBaRlk"
+    agent_id = "3xP6rEjN7k"
     riddle_solvers = {'cipher': cipher_solver, 'captcha': captcha_solver, 'pcap': pcap_solver, 'server': server_solver}
     submission_inference(riddle_solvers)
     print (riddles_counter, len(riddles_counter))
